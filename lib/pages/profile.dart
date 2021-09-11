6cbbfa66adbed5c7260fd35900e917c42d14663b
citrus-candy <kyuuri11013@gmail.com>
2021-09-11 13:21:54 +0900

    [fix] インデントの修正

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -3,101 +3,109 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      // appBar: new AppBar(
-      //   ),
+    return Scaffold(
       body: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Icon(Icons.account_circle,
-                color: const Color(0xFF000000), size: 48.0),
-            new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.account_circle,
+            color: Color(0xFF000000),
+            size: 48.0
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "山田太郎",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Color(0xFF000000),
+                  fontWeight: FontWeight.w200,
+                  fontFamily: "Roboto"
+                ),
+              ),
+              Text(
+                "3年",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Color(0xFF000000),
+                  fontWeight: FontWeight.w200,
+                  fontFamily: "Roboto"
+                ),
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  new Text(
-                    "山田太郎",
-                    style: new TextStyle(
-                        fontSize: 25.0,
-                        color: const Color(0xFF000000),
-                        fontWeight: FontWeight.w200,
-                        fontFamily: "Roboto"),
+                  Text(
+                    "知り合った人数: ",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"
+                    ),
                   ),
-                  new Text(
-                    "3年",
-                    style: new TextStyle(
-                        fontSize: 15.0,
-                        color: const Color(0xFF000000),
-                        fontWeight: FontWeight.w200,
-                        fontFamily: "Roboto"),
+                  Text(
+                    "100人",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"
+                    ),
                   ),
-                  new Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        new Text(
-                          "知り合った人数: ",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: const Color(0xFF000000),
-                              fontWeight: FontWeight.w200,
-                              fontFamily: "Roboto"),
-                        ),
-                        new Text(
-                          "100人",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: const Color(0xFF000000),
-                              fontWeight: FontWeight.w200,
-                              fontFamily: "Roboto"),
-                        ),
-                      ]),
-                  new Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        new Text(
-                          "タグ: ",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: const Color(0xFF000000),
-                              fontWeight: FontWeight.w200,
-                              fontFamily: "Roboto"),
-                        ),
-                        new Text(
-                          "プログラマー",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: const Color(0xFF000000),
-                              fontWeight: FontWeight.w200,
-                              fontFamily: "Roboto"),
-                        ),
-                      ]),
-
-                      new Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-
-                        new Text(
-                          "こんにちは！知的システム工学科４年の太郎です！",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: const Color(0xFF000000),
-                              fontWeight: FontWeight.w200,
-                              fontFamily: "Roboto"),
-                        ),
-                      ])
-                ]),
-          ])
-          
-          
+                ]
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "タグ: ",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"
+                    ),
+                  ),
+                  Text(
+                    "プログラマー",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"),
+                  ),
+                ]
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "こんにちは！知的システム工学科４年の太郎です！",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"
+                    ),
+                  ),
+                ]
+              )
+            ]
+          ),
+        ]
+      )  
     );
   }
 }
