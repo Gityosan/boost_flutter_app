03641e7ceb7e74ab464f50f6d3827488c5ac9064
citrus-candy <kyuuri11013@gmail.com>
2021-09-11 14:53:44 +0900

    [fix] プロフィールのレイアウトの修正

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,111 +1,79 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
+  static const userImage = "https://cdn-images-1.medium.com/max/1200/1*ilC2Aqp5sZd1wi0CopD1Hw.png";
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Icon(
-            Icons.account_circle,
-            color: Color(0xFF000000),
-            size: 48.0
+      body: Column(
+        children: [
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.all(20),
+            color: Colors.lightBlueAccent,
+            child: Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 80.0,
+                        height: 80.0,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 1.0,
+                              blurRadius: 10.0,
+                              offset: Offset(5, 5),
+                            ),
+                          ],
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black, width: 2),
+                          color: Colors.white,
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(userImage)
+                          )
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "山田太郎",
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w200,
+                            ),
+                          ),
+                          Text("3年"),
+                          Text("知り合った人数：100人"),
+                          Text("タグ：プログラマー"),
+                        ]
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text("一般的には、人物の経歴や紹介のことを指すが、コンピューター分野では、コンピューターを構成する機器の種類や搭載されたOSのバージョンなどの情報のことを指す。")
+                ]
+              ),
+            )
           ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "山田太郎",
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Color(0xFF000000),
-                  fontWeight: FontWeight.w200,
-                  fontFamily: "Roboto"
-                ),
-              ),
-              Text(
-                "3年",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Color(0xFF000000),
-                  fontWeight: FontWeight.w200,
-                  fontFamily: "Roboto"
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "知り合った人数: ",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                  Text(
-                    "100人",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                ]
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "タグ: ",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                  Text(
-                    "プログラマー",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"),
-                  ),
-                ]
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "こんにちは！知的システム工学科４年の太郎です！",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                ]
-              )
-            ]
-          ),
-        ]
-      )  
+            children: [
+              Text("過去に開催したイベント"),
+              Text("過去に参加したイベント"),
+            ],
+          )
+        ],
+      )
+
+      
     );
   }
 }
